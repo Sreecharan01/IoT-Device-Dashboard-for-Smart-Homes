434f7b8cde3935705942303c488dbac8b6170a70
Sreecharan01 <150925486+Sreecharan01@users.noreply.github.com>
2026-05-18 15:13:44 +0530

    Relax CORS policy

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -19,9 +19,9 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:5173')],
+    'allowed_origins' => [env('FRONTEND_URL')],
 
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => ['#^https?://#'],
 
     'allowed_headers' => ['*'],
 
